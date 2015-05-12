3ade1d6ab75e0ab49dfc56ab7e2657da4e24887f
unknown <inmi>
2015-05-12 17:46:02 +0200

    added Model Classes

--- src/main/java/ch/fhnw/oop2/academyApp/AAController.java
+++ src/main/java/ch/fhnw/oop2/academyApp/AAController.java
@@ -3,5 +3,15 @@ package ch.fhnw.oop2.academyApp;
 /**
  * Created by Michael on 5/12/2015.
  */
-public class AAController {
+public class AAController extends AAView {
+
+    private AAModel aamodel;
+    private AAView aaview;
+
+    public static void main(String[] args) {
+
+
+
+
+    }
 }
